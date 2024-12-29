b1890b9b8b6c2ab85d80c95f299f7c03e5429fb7
DEKU9873 <alhassan9873@gmail.com>
2024-12-29 08:07:57 +0300

    second commit

--- products/filters.py
+++ products/filters.py
@@ -2,7 +2,8 @@ import django_filters
 from .models import Product
 class ProductFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_expr='iexact')
+    keyword = django_filters.CharFilter(field_name='title', lookup_expr='icontains')
 
     class Meta:
         model = Product
-        fields = ['title', 'brand', 'category', 'price']
+        fields = ['title', 'brand', 'category', 'price', 'keyword']
